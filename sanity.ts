280a4d928895518f787ed99aa3d664217f9fe35d
Mateusz Fidura <mateuszfidura@Mac.home>
2026-04-10 23:18:31 +0200

    Add fallback for SANITY_PROJECT_ID to fix build error

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from '@sanity/image-url';
 export const config = {
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 	// projectId: '8l3ahm8d',
-	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '8l3ahm8d',
 	apiVersion: '2021-03-25',
 	useCdn: process.env.NODE_ENV === 'production',
 };
